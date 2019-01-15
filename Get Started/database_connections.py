492f70329d19d1e0e60e47a489ee46e1c86553ab
Tinels972 <nelson.allain@gmail.com>
2019-01-15 00:56:06 +0000

    remove useless example scripts

--- Get Started/database_connections.py
+++ Get Started/database_connections.py
@@ -6,15 +6,12 @@ GENERAL PURPOSE
 
 This script will aim to show how to store data into a database - nothing more
 
-The sequel to this script (database_connections_2.py) will show how to retrieve data from the same db
-and then display it using the visual capabilities of the AReS library
-
-Finally database_connection_3.py will show a simple example of data extraction using the AReS connectors capabilities 
+Finally database_connection_2.py will show a simple example of data extraction using the AReS connectors capabilities 
 (see the scripts example connectors.py for more info),do some simple transformations, store it in the database. 
 The aim of this module is to show a quick example on retreiving
 the data directly from the db if it's there or go through the data extraction step if it's missing
 
-After going through the three examples you should have the basics to use databases within AReS
+After going through these examples you should have the basics to use databases within AReS
 
 
 PRE-REQUISITE
